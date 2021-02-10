032f1698abf462056a9de95f641c98e60d1612db
Qing-Long Zhang <loukia@msn.cn>
2021-02-10 16:31:16 +0800

    Update sa_resnet.py
    remove sa_resnet18 and sa_resnet34

--- models/sa_resnet.py
+++ models/sa_resnet.py
@@ -68,50 +68,6 @@ def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
-
-class SABasicBlock(nn.Module):
-    expansion = 1
-    __constants__ = ['downsample']
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None):
-        super(SABasicBlock, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1 or base_width != 64:
-            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
-        if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.sa = sa_layer(planes * 4)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.sa(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
 class SABottleneck(nn.Module):
     expansion = 4
     __constants__ = ['downsample']
@@ -253,17 +209,7 @@ class ResNet(nn.Module):
         x = self.fc(x)
 
         return x
-
-
-def sa_resnet18(num_classes=1000, pretrained=False):
-    model = ResNet(SABasicBlock, [2, 2, 2, 2], num_classes)
-    return model
-
-
-def sa_resnet34(num_classes=1000, pretrained=False):
-    model = ResNet(SABasicBlock, [3, 4, 6, 3], num_classes)
-    return model
-
+    
 
 def sa_resnet50(num_classes=1000, pretrained=False):
     print("Constructing csg_resnet50......")
