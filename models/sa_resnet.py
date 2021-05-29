fbd024bd9cd93db22ddf7c47417bb69ddbb04435
wofmanaf <wofmanaf@gmail.com>
2021-05-29 15:40:55 +0800

    update readme

--- models/sa_resnet.py
+++ models/sa_resnet.py
@@ -124,7 +124,6 @@ class SABottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-
     def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
                  norm_layer=None):
